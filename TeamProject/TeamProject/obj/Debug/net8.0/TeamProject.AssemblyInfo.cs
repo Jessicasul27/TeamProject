94936be689ae8830bef5bf2325767cdf12a95ab3
JessicaSul27 <L00183514@atu.ie#>
2026-02-12 21:15:02 +0000

    Added temporary bootswatch theme using Bootswatch website to add a admininstration dropdown which will later contain CRUD privledges for admin

--- TeamProject/TeamProject/obj/Debug/net8.0/TeamProject.AssemblyInfo.cs
+++ TeamProject/TeamProject/obj/Debug/net8.0/TeamProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d1e2385a4758363a2d11be04c95f5d85dc3f09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e0a6e7d551db321e235473a0be8cbc8f7db4b0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
