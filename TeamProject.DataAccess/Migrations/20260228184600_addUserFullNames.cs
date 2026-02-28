5de7ade2ff2841c1caad6124699abc4994aeeee1
katazina <katazinabz003523@gmail.com>
2026-02-28 19:06:22 +0000

    regenerate migrations

--- TeamProject.DataAccess/Migrations/20260228184600_addUserFullNames.cs
+++ TeamProject.DataAccess/Migrations/20260228184600_addUserFullNames.cs
@@ -5,7 +5,7 @@
 namespace TeamProject.DataAccess.Migrations
 {
     /// <inheritdoc />
-    public partial class ver1 : Migration
+    public partial class addUserFullNames : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
