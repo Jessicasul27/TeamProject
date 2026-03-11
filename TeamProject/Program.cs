b4706fc7690d975d9256b3629a4b63a5ca694e62
Ryan Turner <L00183767@atu.ie>
2026-03-11 19:25:38 +0000

    Finished User Based Layouts and fixed the layout of the CRUDs

--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -52,6 +52,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
