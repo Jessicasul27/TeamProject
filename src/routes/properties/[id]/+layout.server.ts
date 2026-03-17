f6a5265f28b582ee02c1b1d2192ceb4fa06c6e02
ummm-pickle-duck <L00178222@atu.ie>
2026-03-17 19:06:40 +0000

    show an error page if a property is not found

--- src/routes/properties/[id]/+layout.server.ts
+++ src/routes/properties/[id]/+layout.server.ts
@@ -1,14 +1,19 @@
 import { db } from "$lib/server/db";
+import { error } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ params }) => {
-  const property = await db.propertyRepo.findOneOrFail({
+  const property = await db.propertyRepo.findOne({
     where: { id: params.id },
     relations: {
       images: true,
       landlord: true,
     },
   });
+
+  if (!property) {
+    throw error(404, `Property with id '${params.id}' was not found.`);
+  }
 
   return {
     property: structuredClone(property),
