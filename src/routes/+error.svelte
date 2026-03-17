56b273b090bb86908eb9737f16b347aa21c84ebb
ummm-pickle-duck <L00178222@atu.ie>
2026-03-17 18:56:47 +0000

    simple error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { page } from "$app/state";
+</script>
+
+<div class="flex flex-col items-center gap-2">
+  <h1 class="text-4xl font-semibold">{page.status}</h1>
+  <p class="text-xl">{page.error?.message}</p>
+</div>
